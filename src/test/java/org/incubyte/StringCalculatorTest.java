2d2fd228d79599f3d2a9e2d8bfdaea1d319bcfa6
Deep Govindvira <deepgovindvira@gmail.com>
2025-07-04 01:45:32 +0530

    refactor: use static imports for assertions

--- src/test/java/org/incubyte/StringCalculatorTest.java
+++ src/test/java/org/incubyte/StringCalculatorTest.java
@@ -1,6 +1,8 @@
 package org.incubyte;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -15,41 +17,41 @@ public class StringCalculatorTest {
 
     @Test
     public void add_givenEmptyString_returnsZero() {
-        Assertions.assertEquals(0, stringCalculator.add(""));
+        assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
     public void add_givenSingleNumber_returnsItSelf() {
-        Assertions.assertEquals(5, stringCalculator.add("5"));
+        assertEquals(5, stringCalculator.add("5"));
     }
 
     @Test
     public void add_givenTwoCommaSeparatedNumbers_returnsAdditionOfBoth() {
-        Assertions.assertEquals(3, stringCalculator.add("1,2"));
+        assertEquals(3, stringCalculator.add("1,2"));
     }
 
     @Test
     void add_givenMultipleCommaSeparatedNumbers_returnsAdditionOfAll() {
-        Assertions.assertEquals(28, stringCalculator.add("6,3,7,8,4"));
+        assertEquals(28, stringCalculator.add("6,3,7,8,4"));
     }
 
     @Test
     void add_givenCommaAndNewlineSeparatedNumbers_returnsAdditionOfAll() {
-        Assertions.assertEquals(65, stringCalculator.add("4\n8,5\n3,45"));
+        assertEquals(65, stringCalculator.add("4\n8,5\n3,45"));
     }
 
     // format: "//[delimiter]\n[numbers]".
     @Test
     void add_givenCustomDelimiter_returnsAdditionOfAll() {
-        Assertions.assertEquals(3, stringCalculator.add("//;\n1;2"));
+        assertEquals(3, stringCalculator.add("//;\n1;2"));
     }
 
     @Test
     void add_givenMultipleNegatives_throwsWithAllInMessage() {
-        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             stringCalculator.add("1,-2,3,-4");
         });
 
-        Assertions.assertEquals("negatives not allowed: [-2, -4]", exception.getMessage());
+        assertEquals("negatives not allowed: [-2, -4]", exception.getMessage());
     }
 }
